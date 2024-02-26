57284a10d64af570ea6d1f4ab5ad7258b0f1beba
ShashankBice <sbaglapl@uw.edu>
2024-02-26 15:33:19 -0800

    change command line script to download DEM

--- download_global_DEM.py
+++ download_global_DEM.py
@@ -6,7 +6,9 @@ from opentopo_dem import opentopo_utils
 
 def get_parser():
         parser = argparse.ArgumentParser(description="utility to download global DEMs from opentopo API for a given extent")
-        dem_options = ['SRTMGL3', 'SRTMGL1', 'SRTMGL1_E', 'AW3D30', 'AW3D30_E', 'SRTM15Plus', 'NASADEM', 'COP30', 'COP90', 'EU_DTM', 'GEDI_L3']
+        dem_options = ['SRTMGL3', 'SRTMGL3_E', 'SRTMGL1', 'SRTMGL1_E', 'AW3D30', 
+                       'AW3D30_E', 'SRTM15Plus', 'SRTM15Plus_E', 'NASADEM', 'NASADEM_E', 
+                       'COP30', 'COP30_E', 'COP90', 'COP90_E', 'EU_DTM', 'EU_DTM_E', 'GEDI_L3']
         parser.add_argument('-demtype',type=str,default='COP30', \
                             choices=dem_options,help='Select the DEM intended to be downloaded (default: %(default)s)')
         parser.add_argument('-extent',help="Bounding box extent in single quotes as 'minx miny maxx maxy' in EPSG:4326 (latitude and longitude)",
@@ -14,8 +16,9 @@ def get_parser():
         parser.add_argument('-poly_fn',help='Vector dataset filename containing polygon specifying desired extent.',
             type=str,required=False,default=None)
         parser.add_argument('-apikey',help='Opentopgraphy API key',type=str,default='demoapikeyot2022',required=False)
-        parser.add_argument('-out_fn',help='Output filename',type=str,default=None)
-        parser.add_argument('-out_proj',type=str,default='EPSG:4326',help='Output projection string (default: %(default)s)')
+        parser.add_argument('-out_fn',help='Desired output filename, the program appends output horizontal crs EPSG code at the end',type=str,default=None)
+        parser.add_argument('-out_proj',type=str,default=None,help='Output EPSG code for horizontal CRS (e.g., EPSG:4326, EPSG:32610); if not provided, will default to opentopography provided horizontal CRS')
+
         return parser
 
 
